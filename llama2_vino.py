53f6be596e1c757d15e7fa89539addc1c2773f9b
sangbin-sangbin <sangbin.park@intel.com>
2024-01-16 15:30:59 +0900

    1

--- llama2_vino.py
+++ llama2_vino.py
@@ -6,33 +6,27 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 
 
 model_name = "NousResearch/Llama-2-7b-chat-hf"
+model_dir = "../Desktop"
 
 save_model_path = Path('../Desktop/vino_model.xml')
 
 core = ov.Core()
-
 device = widgets.Dropdown(
     options=core.available_devices + ["AUTO"],
-    value='AUTO',
-    description='Device:',
+    value="GPU",
+    description="Device:",
     disabled=False,
 )
-
-compiled_model = core.compile_model(save_model_path, device.value)
-
 
 ov_config = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}
 
 ov_model = OVModelForCausalLM.from_pretrained(
-    model_name,
+    model_dir,
     device=device.value,
     ov_config=ov_config,
-    config=AutoConfig.from_pretrained(model_name, trust_remote_code=True),
+    config=AutoConfig.from_pretrained(model_dir, trust_remote_code=True),
     trust_remote_code=True,
 )
-
-
-
 
 
 fine_tuned_tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, return_dict=True)
@@ -40,8 +34,7 @@ fine_tuned_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 fine_tuned_tokenizer.pad_token = fine_tuned_tokenizer.eos_token
 fine_tuned_tokenizer.padding_side = "right"
 
-while True:
-    text = "How can I buy BMW?"#input("question: ")
-    encoded_input = fine_tuned_tokenizer(text, return_tensors='pt')
-    res = compiled_model.generate(**encoded_input, max_new_tokens=2)
-    print(fine_tuned_tokenizer.batch_decode(res, skip_special_tokens=True)[0])
+text = "How can I buy BMW?"#input("question: ")
+encoded_input = fine_tuned_tokenizer(text, return_tensors='pt')
+res = compiled_model.generate(**encoded_input, max_new_tokens=2)
+print(fine_tuned_tokenizer.batch_decode(res, skip_special_tokens=True)[0])
