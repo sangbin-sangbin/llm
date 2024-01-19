faa11d0eff495844e39f95832cec4736fd8de258
sangbin-sangbin <sangbin.park@intel.com>
2024-01-19 11:45:40 +0900

    !

--- llama2_convert.py
+++ llama2_convert.py
@@ -1,14 +1,36 @@
-from transformers import AutoConfig
 from optimum.intel.openvino import OVModelForCausalLM
 import openvino as ov
 from pathlib import Path
 
 
-model_type = AutoConfig.from_pretrained('../models/llama-2-7b-chat-hf', trust_remote_code=True).model_type
-model_dir = Path('../models/llama2_vino')
+model_name = "../models/llama-2-7b-chat-hf"
+
+new_model = "../models/new-llama2-model"
+
+vino_dir = Path('../models/llama2_vino')
+
+device_map = {"": 0}
+
+# Reload model in FP16 and merge it with LoRA weights
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    device_map=device_map,
+)
+
+fine_tuned_model = PeftModel.from_pretrained(base_model, new_model)
+fine_tuned_model = fine_tuned_model.merge_and_unload()
+
+# Reload tokenizer to save it
+fine_tuned_tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+fine_tuned_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+fine_tuned_tokenizer.pad_token = fine_tuned_tokenizer.eos_token
+fine_tuned_tokenizer.padding_side = "right"
 
 ov_model = OVModelForCausalLM.from_pretrained(
-    '../models/llama-2-7b-chat-hf', export=True, compile=False
+    fine_tuned_model, export=True, compile=False
 )
 ov_model.half()
-ov_model.save_pretrained(model_dir) 
+ov_model.save_pretrained(vino_dir) 
