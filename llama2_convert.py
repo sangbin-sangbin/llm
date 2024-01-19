743d1c000fef2e5752fd15e883baf586d36db55d
sangbin-sangbin <sangbin.park@intel.com>
2024-01-19 16:05:27 +0900

    1

--- llama2_convert.py
+++ llama2_convert.py
@@ -11,10 +11,8 @@ vino_dir = Path('../models/llama2_vino')
 
 device_map = {"": 0}
 
-print('start')
 ov_model = OVModelForCausalLM.from_pretrained(
     new_full_model, export=True, compile=False
 )
-print('end')
 ov_model.half()
 ov_model.save_pretrained(vino_dir) 
