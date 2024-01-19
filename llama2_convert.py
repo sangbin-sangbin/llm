41a42d7852eb57e2cff3ddca8a2592acf8d6be5e
sangbin-sangbin <sangbin.park@intel.com>
2024-01-19 11:14:33 +0900

    1

--- llama2_convert.py
+++ llama2_convert.py
@@ -8,39 +8,18 @@ import torch
 import logging
 import nncf
 import gc
-from converter import converters
 from config import SUPPORTED_LLM_MODELS
+
 
 model_configuration = SUPPORTED_LLM_MODELS['llama-2-chat-7b']
 nncf.set_log_level(logging.ERROR)
 
-pt_model_id = model_configuration["model_id"]
 pt_model_name = 'llama-2-chat-7b'.split("-")[0]
 model_type = AutoConfig.from_pretrained('../models/llama-2-7b-chat-hf', trust_remote_code=True).model_type
-fp16_model_dir = Path('../models')
+fp16_model_dir = Path('../models/llama2_vino')
 
-def convert_to_fp16():
-    if (fp16_model_dir / "openvino_model.xml").exists():
-        return
-    if not model_configuration["remote"]:
-        ov_model = OVModelForCausalLM.from_pretrained(
-            '../models/llama-2-7b-chat-hf', export=True, compile=False
-        )
-        ov_model.half()
-        ov_model.save_pretrained(fp16_model_dir)
-        del ov_model
-    else:
-        model_kwargs = {}
-        if "revision" in model_configuration:
-            model_kwargs["revision"] = model_configuration["revision"]
-        model = AutoModelForCausalLM.from_pretrained(
-            '../models/llama-2-7b-chat-hf',
-            torch_dtype=torch.float32,
-            trust_remote_code=True,
-            **model_kwargs
-        )
-        converters[pt_model_name](model, fp16_model_dir)
-        del model
-    gc.collect()
-
-convert_to_fp16()
+ov_model = OVModelForCausalLM.from_pretrained(
+    '../models/llama-2-7b-chat-hf', export=True, compile=False
+)
+ov_model.half()
+ov_model.save_pretrained(fp16_model_dir)
--- llama2_vino.py
+++ llama2_vino.py
@@ -1,60 +1,28 @@
-from ov_llm_model import model_classes
+from transformers import AutoTokenizer, AutoConfig
+import openvino as ov
+from pathlib import Path
+from optimum.intel.openvino import OVModelForCausalLM
+from config import SUPPORTED_LLM_MODELS
 
 
 core = ov.Core()
-device = widgets.Dropdown(
-    options=core.available_devices + ["AUTO"],
-    value="CPU",
-    description="Device:",
-    disabled=False,
-)
+device = "CPU"
+model_dir = Path('../models/llama2_vino')
 
-available_models = []
-if int4_model_dir.exists():
-    available_models.append("INT4")
-if int8_model_dir.exists():
-    available_models.append("INT8")
-if fp16_model_dir.exists():
-    available_models.append("FP16")
-
-model_to_run = widgets.Dropdown(
-    options=available_models,
-    value=available_models[0],
-    description="Model to run:",
-    disabled=False,
-)
-
-from transformers import AutoTokenizer
-
-if model_to_run.value == "INT4":
-    model_dir = int4_model_dir
-elif model_to_run.value == "INT8":
-    model_dir = int8_model_dir
-else:
-    model_dir = fp16_model_dir
-print(f"Loading model from {model_dir}")
-
-model_name = model_configuration["model_id"]
-class_key = model_id.value.split("-")[0]
 ov_config = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}
 
-tok = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tok = AutoTokenizer.from_pretrained('../models/llama-2-7b-chat-hf', trust_remote_code=True)
 
-model_class = (
-    OVModelForCausalLM
-    if not model_configuration["remote"]
-    else model_classes[class_key]
-)
-ov_model = model_class.from_pretrained(
+ov_model = OVModelForCausalLM.from_pretrained(
     model_dir,
-    device=device.value,
+    device=device,
     ov_config=ov_config,
     config=AutoConfig.from_pretrained(model_dir, trust_remote_code=True),
     trust_remote_code=True,
 )
 
-tokenizer_kwargs = model_configuration.get("tokenizer_kwargs", {})
-test_string = "2 + 2 ="
-input_tokens = tok(test_string, return_tensors="pt", **tokenizer_kwargs)
-answer = ov_model.generate(**input_tokens, max_new_tokens=2)
-print(tok.batch_decode(answer, skip_special_tokens=True)[0])
+while True:
+    test_string = input("question: ")
+    input_tokens = tok(f"<s>[INST] {test_string} [/INST] ", return_tensors="pt", add_special_tokens=False)
+    answer = ov_model.generate(**input_tokens, max_new_tokens=1024)
+    print(tok.batch_decode(answer, skip_special_tokens=True)[0])
