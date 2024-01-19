308e0d86834a8113b24c6b7e62744cd95fac5b1f
sangbin-sangbin <sangbin.park@intel.com>
2024-01-19 14:07:45 +0900

    1

--- llama2_save_full_model.py
+++ llama2_save_full_model.py
@@ -5,6 +5,10 @@ from pathlib import Path
 import torch
 
 
+model_name = "../models/llama-2-7b-chat-hf"
+
+new_model = "../models/new-llama2-model"
+
 new_full_model = "../models/new-llama2-full-model"
 
 vino_dir = Path('../models/llama2_vino')
@@ -12,16 +16,15 @@ vino_dir = Path('../models/llama2_vino')
 device_map = {"": 0}
 
 # Reload model in FP16 and merge it with LoRA weights
-fine_tuned_model = AutoModelForCausalLM.from_pretrained(
-    new_full_model,
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
     low_cpu_mem_usage=True,
     return_dict=True,
     torch_dtype=torch.float16,
     device_map=device_map,
 )
 
-ov_model = OVModelForCausalLM.from_pretrained(
-    fine_tuned_model, export=True, compile=False
-)
-ov_model.half()
-ov_model.save_pretrained(vino_dir) 
+fine_tuned_model = PeftModel.from_pretrained(base_model, new_model)
+fine_tuned_model = fine_tuned_model.merge_and_unload()
+
+fine_tuned_model.save_pretrained(new_full_model)
