64e70549d0628e708cd7252efff40970ad3f865f
sangbin-sangbin <sangbin.park@intel.com>
2024-01-19 14:11:36 +0900

    1

--- llama2_convert.py
+++ llama2_convert.py
@@ -11,17 +11,8 @@ vino_dir = Path('../models/llama2_vino')
 
 device_map = {"": 0}
 
-# Reload model in FP16 and merge it with LoRA weights
-fine_tuned_model = AutoModelForCausalLM.from_pretrained(
-    new_full_model,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    torch_dtype=torch.float16,
-    device_map=device_map,
-)
-
 ov_model = OVModelForCausalLM.from_pretrained(
-    fine_tuned_model, export=True, compile=False
+    new_full_model, export=True, compile=False
 )
 ov_model.half()
 ov_model.save_pretrained(vino_dir) 
