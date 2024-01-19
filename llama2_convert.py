1fe2f432fcd7b1693598ba57c6deb804c6807ca4
sangbin-sangbin <sangbin.park@intel.com>
2024-01-19 11:22:28 +0900

    1

--- llama2_convert.py
+++ llama2_convert.py
@@ -21,4 +21,4 @@ ov_model = OVModelForCausalLM.from_pretrained(
     '../models/llama-2-7b-chat-hf', export=True, compile=False
 )
 ov_model.half()
-ov_model.save_pretrained(model_dir) 
+ov_model.save_pretrained(model_dir)
