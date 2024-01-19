b6b23f46121ae1ec794968616bad001b2ab7c4e0
sangbin-sangbin <sangbin.park@intel.com>
2024-01-19 16:27:19 +0900

    1

--- llama2_convert.py
+++ llama2_convert.py
@@ -7,14 +7,10 @@ import torch
 
 new_full_model = "../models/llama-2-7b-chat-hf"
 
-vino_dir = Path('../models/llama2_vino')
-
 device_map = {"": 0}
 
-print('start')
 ov_model = OVModelForCausalLM.from_pretrained(
     new_full_model, export=True, compile=False
 )
-print('end')
 ov_model.half()
 ov_model.save_pretrained(vino_dir) 
