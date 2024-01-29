5465df10b453e69aea59882df53d889fd187248d
sangbin-sangbin <sangbin.park@intel.com>
2024-01-29 09:58:28 +0900

    !

--- compare_speed.py
+++ compare_speed.py
@@ -11,6 +11,56 @@ import json
 import re
 import time
 
+from transformers import AutoTokenizer, AutoConfig
+import openvino as ov
+from pathlib import Path
+from optimum.intel.openvino import OVModelForCausalLM
+import time
+
+test_strings = [
+    "How can I delete my personal data and settings in my BMW?",
+    "How do I see the last parking position of my BMW?",
+    "Why does my BMW activate the guest profile, even though I have already created a driver profile with my BMW ID?",
+    "How can I air-condition my BMW with the Remote Services of the My BMW App?",
+    "How can I find detailed information on my last trip in the My BMW App?",
+    "How do I delete all stored destinations in the navigation system of my BMW?",
+    "How do I remove a vehicle from the My BMW App?",
+    "How do I create a BMW ID?",
+    "Where can I change the account data for my BMW ID?",
+    "Does the My BMW App or the My BMW portal show me the electronic service history of my BMW?",
+]
+
+base_model = '../models/llama-2-7b-chat-hf'
+model_dir = Path('../models/llama2_vino')
+
+core = ov.Core()
+device = "CPU"
+ov_config = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}
+
+tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+
+ov_model = OVModelForCausalLM.from_pretrained(
+    '../models/llama2_openvino_8',
+    device=device,
+    ov_config=ov_config,
+    config=AutoConfig.from_pretrained(model_dir, trust_remote_code=True),
+    trust_remote_code=True,
+)
+
+total_time_8 = 0
+for test_string in test_strings:
+    input_tokens = tokenizer(f"<s>[INST] {test_string} [/INST] ", return_tensors="pt", add_special_tokens=False)
+    start = time.time()
+    result = ov_model.generate(**input_tokens, max_new_tokens=1024)
+    end = time.time()
+    print(tokenizer.batch_decode(result, skip_special_tokens=True)[0])
+    total_time_8 += end - start
+
+
 
 model_name = "../models/llama-2-7b-chat-hf"
 new_model = '../models/new-llama2-model-llama-aug'
@@ -38,38 +88,6 @@ logging.set_verbosity(logging.CRITICAL)
 
 pipe = pipeline(task="text-generation", model=fine_tuned_model, tokenizer=fine_tuned_tokenizer, max_length=1024)
 
-test_strings = [
-    "Which Remote Services can I use for my vehicle in conjunction with the My BMW App?",
-    "Who are you?",
-    "hello, nice to meet you.",
-    "How can I buy BMW?",
-    "How much is tesla model Y?",
-    "I want to go home.",
-    "I will find you and kill you",
-    "hello my name is sangbin",
-    "please call 911 I am dying",
-    "how do you think about stock of BMW, should I buy it?",
-    "what is lotto number of next week?",
-    "Is a map update available for my BMW?",
-    "How can I activate or deactivate the vehicle tracking of my BMW?",
-    "How can I restart or \"reboot\" the BMW iDrive system in my vehicle?",
-    "How do I track the status of my new BMW vehicle order online?",
-    "How do I activate the \"Remote Engine Start\" function for my BMW?",
-    "How do I log in or out of Spotify via BMW Connected Music in my BMW?",
-    "Which login data can I use to access the My BMW App or the My BMW portal on the web?",
-    "How do I activate the Key Card for my BMW Digital Key?",
-    "How do I add a vehicle to my BMW ID in the My BMW App?",
-    "How can I delete my personal data and settings in my BMW?",
-    "How do I see the last parking position of my BMW?",
-    "Why does my BMW activate the guest profile, even though I have already created a driver profile with my BMW ID?",
-    "How can I air-condition my BMW with the Remote Services of the My BMW App?",
-    "How can I find detailed information on my last trip in the My BMW App?",
-    "How do I delete all stored destinations in the navigation system of my BMW?",
-    "How do I remove a vehicle from the My BMW App?",
-    "How do I create a BMW ID?",
-    "Where can I change the account data for my BMW ID?",
-    "Does the My BMW App or the My BMW portal show me the electronic service history of my BMW?",
-]
 without_vino_total_time = 0
 for test_string in test_strings:
     input_tokens = fine_tuned_tokenizer(f"<s>[INST] {test_string} [/INST] ", return_tensors="pt", add_special_tokens=False)
@@ -87,40 +105,25 @@ for test_string in test_strings:
 
 
 
-from transformers import AutoTokenizer, AutoConfig
-import openvino as ov
-from pathlib import Path
-from optimum.intel.openvino import OVModelForCausalLM
-import time
 
-
-base_model = '../models/llama-2-7b-chat-hf'
-model_dir = Path('../models/llama2_vino')
-
-core = ov.Core()
-device = "CPU"
-ov_config = {"PERFORMANCE_HINT": "LATENCY", "NUM_STREAMS": "1", "CACHE_DIR": ""}
-
-tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
-tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"
 
 ov_model = OVModelForCausalLM.from_pretrained(
-    model_dir,
+    '../models/llama2_openvino_16',
     device=device,
     ov_config=ov_config,
     config=AutoConfig.from_pretrained(model_dir, trust_remote_code=True),
     trust_remote_code=True,
 )
 
-with_vino_total_time = 0
+total_time_16 = 0
 for test_string in test_strings:
     input_tokens = tokenizer(f"<s>[INST] {test_string} [/INST] ", return_tensors="pt", add_special_tokens=False)
     start = time.time()
     result = ov_model.generate(**input_tokens, max_new_tokens=1024)
     end = time.time()
     print(tokenizer.batch_decode(result, skip_special_tokens=True)[0])
-    with_vino_total_time += end - start
+    total_time_16 += end - start
     
-print("\n\nwith_vino_total_time:", with_vino_total_time, "\n\nwithout_vino_total_timee:", without_vino_total_time)
+
+
+print("\n\nwith_vino_total_time:", with_vino_total_time, "\n\n16:", total_time_16, "\n\n8:", total_time_8)
--- llama2_convert.py
+++ llama2_convert.py
@@ -3,15 +3,37 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import openvino as ov
 from pathlib import Path
 import torch
+from optimum.intel import OVQuantizer
+import nncf
+
 
 #new_full_model = 'NousResearch/Llama-2-7b-chat-hf'
 #new_full_model = '../models/llama-2-7b-chat-hf'
 new_full_model = '../models/new-llama2-full-model'
 
-vino_dir = '../models/llama2_vino'
+openvino_4_dir = '../models/llama2_openvino_4'
+openvino_8_dir = '../models/llama2_openvino_8'
+openvino_16_dir = '../models/llama2_openvino_16'
 
 ov_model = OVModelForCausalLM.from_pretrained(
     new_full_model, export=True, compile=False
 )
 ov_model.half()
-ov_model.save_pretrained(vino_dir) 
+
+modelType = input("which type?: [int4 / int8 / fp16]")
+
+if modelType == 'fp16':
+    ov_model.save_pretrained(openvino_16_dir) 
+elif modelType == 'int8':
+    q = OVQuantizer.from_pretrained(ov_model)
+    q.quantize(save_directory=openvino_8_dir, weights_only=True)
+elif modelType == 'int4':
+    model_compression_params = {
+        "mode": nncf.CompressWeightsMode.INT4_SYM,
+        "group_size": 128,
+        "ratio": 0.8,
+    }
+    compressed_model = nncf.compress_weights(ov_model, **model_compression_params)
+    ov.save_model(compressed_model, openvino_4_dir / "openvino_model.xml")
+    shutil.copy(openvino_16_dir / "config.json", openvino_4_dir / "config.json")
+
